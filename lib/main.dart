a06863cb6a10f04e215eda27bbda9b20a872a81b
adomhamza <liveghost93@gmail.com>
2021-10-31 20:58:35 +0000

    Wrapped Main.dart with ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'api/api_service_provider.dart';
 import 'gram_spy_theme.dart';
+import 'route/routes.dart';
 
 void main() {
   runApp(const GramSpy());
@@ -12,9 +15,16 @@ class GramSpy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = GramSpyTheme.light();
-    return MaterialApp(
-      title: 'GramSpy',
-      theme: theme,
+    return ChangeNotifierProvider(
+      create: (context) => APIService(),
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'GramSpy',
+          theme: theme,
+          initialRoute: RouteManager.homePage,
+          onGenerateRoute: RouteManager.generateRoute,
+        );
+      },
     );
   }
 }
