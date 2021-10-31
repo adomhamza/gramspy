ab7f02f3e47d55401d09aa62732e527c5d7d8be6
adomhamza <liveghost93@gmail.com>
2021-10-31 20:04:38 +0000

    Form UI

---
+++ lib/screens/form.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+import '../gram_spy_theme.dart';
+
+class Form extends StatefulWidget {
+  const Form({Key? key}) : super(key: key);
+
+  @override
+  State<Form> createState() => _FormState();
+}
+
+class _FormState extends State<Form> {
+  final _usernamecontroller = TextEditingController();
+  String? get emptyText {
+    final text = _usernamecontroller.value.text;
+    if (text.isEmpty) {
+      return 'Enter a valid InstaGram username';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: AnimatedBuilder(
+            animation: _usernamecontroller,
+            builder: (context, _) {
+              return Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 100.0),
+                    child: Center(
+                      child: Text(
+                        'GramSpy',
+                        style: GramSpyTheme.lightTextTheme.headline2,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                      height: 100,
+                      child: ListView(
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: const EdgeInsets.only(
+                              left: 30.0,
+                              right: 30.0,
+                            ),
+                            alignment: Alignment.center,
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      errorText: emptyText,
+                                      errorStyle:
+                                          GramSpyTheme.lightTextTheme.bodyText1,
+                                    ),
+                                    autofocus: true,
+                                    textAlign: TextAlign.center,
+                                    style:
+                                        GramSpyTheme.lightTextTheme.headline3,
+                                    controller: _usernamecontroller,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(
+                        left: 30.0, right: 30.0, top: 30.0),
+                    alignment: Alignment.center,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.0)),
+                            ),
+                            onPressed: emptyText == null ? () {} : null,
+                            child: const Text(
+                              'SEARCH',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 15.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
