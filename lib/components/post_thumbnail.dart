b87477a29b24bb6b5d387203e577b808334b0e3e
adomhamza <liveghost93@gmail.com>
2021-10-31 22:02:55 +0000

    PostThumbnail

---
+++ lib/components/post_thumbnail.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../gram_spy_theme.dart';
+
+class PostThumbnail extends StatelessWidget {
+  const PostThumbnail({
+    Key? key,
+    required this.networkImage,
+    required this.commentCount,
+    required this.likesCount,
+  }) : super(key: key);
+  final String networkImage;
+  final String likesCount;
+  final String commentCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              networkImage,
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Text(
+                  likesCount,
+                  style: GramSpyTheme.lightTextTheme.bodyText1,
+                ),
+                const Text(
+                  'Likes',
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  commentCount,
+                  style: GramSpyTheme.lightTextTheme.bodyText1,
+                ),
+                const Text(
+                  'Comments',
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
