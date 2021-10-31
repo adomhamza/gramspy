417eb298d5bd8ca63efdb802975e98f6d3654602
adomhamza <liveghost93@gmail.com>
2021-10-31 21:28:13 +0000

    CircleImage and ProfileStatistics components added

--- lib/screens/response_page.dart
+++ lib/screens/response_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/components.dart';
 import '../api/api_service_provider.dart';
 import '../route/routes.dart';
 import 'package:provider/provider.dart';
@@ -8,25 +9,63 @@ class ResponsePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<APIService>(builder: (context, value, child) {
-      final objectResult = value.userRequestModel!.graphql!;
-      return Scaffold(
+    return Consumer<APIService>(
+      builder: (context, value, child) {
+        final objectResult = value.userRequestModel!.graphql!;
+        return Scaffold(
           appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).pushReplacementNamed(
-                  RouteManager.homePage,
-                );
-                context.read<APIService>().btnLabel = 'SEARCH';
-              },
-              icon: const Icon(
-                Icons.logout_rounded,
-              ))
-        ],
-        elevation: 0.0,
-        title: Text(objectResult.user!.username),
-      ));
-    });
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacementNamed(
+                      RouteManager.homePage,
+                    );
+                    context.read<APIService>().btnLabel = 'SEARCH';
+                  },
+                  icon: const Icon(
+                    Icons.logout_rounded,
+                  ))
+            ],
+            elevation: 0.0,
+            title: Text(objectResult.user!.username),
+          ),
+          body: RefreshIndicator(
+            onRefresh: () {
+              return context
+                  .read<APIService>()
+                  .userRequest(context, objectResult.user!.username);
+            },
+            child: ListView(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleImage(
+                          imageRadius: 50,
+                          networkImage:
+                              NetworkImage(objectResult.user!.profilePicUrlHd),
+                        ),
+                        ProfileStatisics(
+                          postCount: objectResult
+                              .user!.edgeOwnerToTimelineMedia!.count
+                              .toString(),
+                          followersCount: objectResult
+                              .user!.edgeFollowedBy!.count
+                              .toString(),
+                          followingCount:
+                              objectResult.user!.edgeFollow!.count.toString(),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
