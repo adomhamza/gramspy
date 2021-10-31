fd3533b32d861f79a28b048ef0b0a93bf18bb7a8
adomhamza <liveghost93@gmail.com>
2021-10-31 21:11:48 +0000

    Renamed resultpage to responsePage

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -4,13 +4,13 @@ import '../screens/response_page.dart';
 
 class RouteManager {
   static const String homePage = '/';
-  static const String resultDisplayPage = '/result_display';
+  static const String responsePage = '/response_page';
 
   static Route<dynamic> generateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case homePage:
         return MaterialPageRoute(builder: (context) => const FormPage());
-      case resultDisplayPage:
+      case responsePage:
         return MaterialPageRoute(builder: (context) => const ResponsePage());
       default:
         throw const FormatException('No route found');
